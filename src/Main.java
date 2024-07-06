c41db02931d86523c8278d2e4f50e432c99e6944
ElovikovStepan <selovikov12345@gmail.com>
2024-07-06 18:38:37 +0500

    Подключение к базе данных предметов STALCRAFT
    Реализация подключения к API, чтения JSON-файлов, вывода категорий оружия в консоль

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,32 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class Main {
     public static void main(String[] args) {
-
+        JsonArray mainFolder = GetJsonArrayFromString("https://api.github.com/repos/EXBO-Studio/stalcraft-database/contents/ru/items/weapon");
+        PrintJsonArray(mainFolder, "name");
+    }
+    public static JsonArray GetJsonArrayFromString(String sUrl) {
+        try {
+            URLConnection urlConnection = new URL(sUrl).openConnection();
+            urlConnection.connect();
+            JsonParser jsonParser = new JsonParser();
+            JsonArray jsonArray = jsonParser.parse(new InputStreamReader((InputStream) urlConnection.getContent())).getAsJsonArray();
+            return jsonArray;
+        }
+        catch (Exception exp) {
+            return null;
+        }
+    }
+    public static void PrintJsonArray(JsonArray jsonArray, String parameter) {
+        for (JsonElement jsonElement : jsonArray) {
+            System.out.println(jsonElement.getAsJsonObject().get(parameter).toString());
+        }
     }
 }
