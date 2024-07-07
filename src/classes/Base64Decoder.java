fd33cdce93901db8ad6b65d05662255f4b8208ba
ElovikovStepan <selovikov12345@gmail.com>
2024-07-07 11:55:16 +0500

    Улучшение декодирования Base64
    Изменен класс Base64Decoder, возвращаемое значение имеет формат JsonObject

--- src/classes/Base64Decoder.java
+++ src/classes/Base64Decoder.java
@@ -1,14 +1,19 @@
 package classes;
 
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.util.Base64;
 
+/* Класс для расшифровки base64 и преобразования расшифрованного значения в JsonObject */
 public class Base64Decoder {
-    public static String DecryptContent(String encryptedValue) {
+    /* Метод для расшифровки base64 и преобразования расшифрованного значения в JsonObject */
+    public static JsonObject DecryptContent(String encryptedValue) {
         try {
-            String test = encryptedValue.replace("\\n", "");
-            byte[] decodeBytes = Base64.getDecoder().decode(test);
-            String encryptedTest = new String(decodeBytes);
-            return encryptedTest;
+            byte[] decryptBytesFromValue = Base64.getDecoder().decode(encryptedValue.replace("\\n", ""));
+            String decryptedValue = new String(decryptBytesFromValue);
+            JsonObject jsonObject = JsonParser.parseString(decryptedValue).getAsJsonObject();
+            return jsonObject;
         }
         catch (Exception exp) {
             return null;
